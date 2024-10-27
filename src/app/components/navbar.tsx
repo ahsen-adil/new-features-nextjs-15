8caa0f0befb23ee8f90e495dced91cc25219f0b6
ahsen adil <meoahsan01@gmail.com>
2024-10-27 15:18:14 +0500

    update

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Navbar() {
     return (
       <>
-       <header className="text-gray-600 body-font">
+       <header className="bg-gradient-to-r from-blue-100 to-white text-gray-600 body-font">
   <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
       
